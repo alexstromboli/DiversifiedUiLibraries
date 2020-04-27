6c1b2e5b411d3cb1b0651447e0e9482310a7c4c2
Alexey Piskarev <stromboli@yandex.ru>
2020-04-27 18:05:31 -0400

    Generic React app

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>test</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var path = require("path");
+var src = path.join (__dirname, "src");
+var build = path.join (__dirname, "build", "demo");
+//const webpack = require('webpack');
+
+var html_webpack_plugin = require ("html-webpack-plugin");
+
+module.exports =
+{
+	entry:
+	{
+		"index": "./src/app.tsx"
+	},
+	resolve:
+	{
+		extensions: ['.ts', '.tsx', '.js', '.jsx']
+	},
+	output:
+	{
+		path: build,
+		filename: "[name].[hash].js"
+	},
+	plugins:
+	[
+		new html_webpack_plugin ({ title: "index.html", template: path.join (src, "template.html") })
+	],
+	module:
+	{
+		rules:
+		[
+			{
+				test: /\.tsx?$/,
+				use: [{loader: "babel-loader"}, {loader: "ts-loader"}]
+			},
+			{
+				test: /\.jsx?$/,
+				use: 'babel-loader'
+			}
+		]
+	}
+};
