565217adb8f5a5d547062c5c1db3018524d784a3
Alexey Piskarev <stromboli@yandex.ru>
2020-04-28 05:34:49 -0400

    Navigation bar entries adjusted

--- src/app.tsx
+++ src/app.tsx
@@ -27,16 +27,22 @@ class App extends React.Component<List, AppState>
 
   menu_link (mode: Mode, display: string)
   {
-    return <li
-        style={{padding: "1em", backgroundColor: this.state.current == mode ? "white" : "transparent" }}
-      ><a href="#" onClick={e => {e.preventDefault(); this.setState ({...this.state, current: mode}); }}>{display}</a></li>;
+    return <li><a href="#"
+        style={
+          {
+            display: "inline-block",
+            padding: "1em",
+            width: "7em",
+            backgroundColor: this.state.current == mode ? "white" : "transparent"
+          }}
+        onClick={e => {e.preventDefault(); this.setState ({...this.state, current: mode}); }}>{display}</a></li>;
   }
 
   render ()
   {
     return [
-      <div id="left_panel" style={{float: "left", width: "200px", height: "90vh", padding: "1em", backgroundColor: "#f2f2f2"}}>
-        <style>{"li {display: block;} a:link, a:visited, a:hover, a:active {	color: blue; text-decoration: none; }"}</style>
+      <div id="left_panel" style={{float: "left", width: "150px", height: "90vh", padding: "1em", backgroundColor: "#f2f2f2"}}>
+        <style>{"ul {padding: 0} li {display: block;} a:link, a:visited, a:hover, a:active {color: blue; text-decoration: none;}"}</style>
         <ul>
           { this.menu_link (Mode.BareHtml, "Bare HTML") }
           { this.menu_link (Mode.KendoUI, "Kendo UI") }
